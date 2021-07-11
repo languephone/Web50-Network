c9886b5b4568a18d4220f791afb27411ea8427e6
languephone <73309519+languephone@users.noreply.github.com>
2021-07-11 21:37:48 +0100

    Added form to create posts.

--- network/views.py
+++ network/views.py
@@ -4,11 +4,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+
+    # Return all posts
+    posts = Post.objects.all()
+
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
@@ -61,14 +67,3 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
-
-
-def posts(request):
-    if request.method == "GET":
-
-        # Return all posts
-        posts = Post.objects.all()
-
-        return render(request, "network/posts.html", {
-            "posts": posts
-        })
