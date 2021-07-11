709e44277d5e516f1a73c6443b1f9d16bad86688
languephone <73309519+languephone@users.noreply.github.com>
2021-07-11 23:24:23 +0100

    Create Like model.

--- network/models.py
+++ network/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     pass
 
+
 class Post(models.Model):
     content = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,3 +13,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f"Post by {self.user.username}: '{self.content[:20]}...'"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
