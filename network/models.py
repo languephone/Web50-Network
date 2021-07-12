b2d348d9542f076707810b49bb2e43ad58992733
languephone <73309519+languephone@users.noreply.github.com>
2021-07-12 13:50:18 +0100

    Update string defs for Post and Like models.

--- network/models.py
+++ network/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Post by {self.user.username}: '{self.content[:20]}...'"
+        return f"Post by {self.user.username}: '{self.content[:25]}...'"
 
     def count_likes(self):
         likes = len(Like.objects.filter(post=self.id))
@@ -23,3 +23,6 @@ class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} likes '{self.post.content[:25]}...'"
