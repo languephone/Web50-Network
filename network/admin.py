e2bc18ccfe6b96e8cff2e07f73225d90e2b2ce12
languephone <73309519+languephone@users.noreply.github.com>
2021-07-16 17:28:36 +0100

    Add LikeAdmin class to admin.py

--- network/admin.py
+++ network/admin.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from .models import User, Post, Like
 
 # Register your models here.
+class LikeAdmin(admin.ModelAdmin):
+	list_display = ("user", "post", "date")
+
 admin.site.register(User)
 admin.site.register(Post)
-admin.site.register(Like)
+admin.site.register(Like, LikeAdmin)
