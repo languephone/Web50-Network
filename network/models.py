44c45133006586af7d4eb92a806fee8035baa1d1
languephone <73309519+languephone@users.noreply.github.com>
2021-07-19 22:16:17 +0100

    Create 'toggle_like' method for posts.

--- network/models.py
+++ network/models.py
@@ -18,11 +18,27 @@ class Post(models.Model):
         likes = len(Like.objects.filter(post=self.id))
         return likes
 
+    def toggle_like(self, user):
+        # First check if likelist already exists for user, then add/remove post
+        if Like.objects.filter(user=user).exists():
+            likelist = Like.objects.get(user=user)
+            if self not in likelist.post.all():
+                likelist.post.add(self)
+            else:
+                likelist.post.remove(self)
+        # If likelist for user doesn't exist, create one then add post to list
+        else:
+            likelist = Like(user=user)
+            likelist.save()
+            likelist.post.add(self)
+
+
 
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ManyToManyField(Post, blank=True)
     date = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"{self.user.username} likelist"
