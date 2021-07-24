fcba4c94cda6c5668281213d458c46ee200e629f
languephone <73309519+languephone@users.noreply.github.com>
2021-07-24 16:55:02 +0100

    Changed post model to not change datetime on update.

--- network/models.py
+++ network/models.py
@@ -31,7 +31,7 @@ class User(AbstractUser):
 class Post(models.Model):
     content = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateTimeField(auto_now=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Post by {self.user.username}: '{self.content[:25]}...'"
